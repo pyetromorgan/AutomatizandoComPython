6bf49f61ce6ff3abda336bb42b1ea2cd86605a61
Pyetro <pyetromorgan4@gmail.com>
2026-07-09 12:51:00 -0300

    Mexendo com a biblioteca pandas, lendo uma base de dados e automatizando

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import pandas
 
 url = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
@@ -17,3 +18,34 @@ pyautogui.write("12345testador2")
 pyautogui.press("tab")
 pyautogui.press("enter")
 time.sleep(2)
+tabela = pandas.read_csv("produtos.csv")
+print(tabela)
+
+for linha in tabela.index:
+    pyautogui.click(x=692, y=293)
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(str(marca))
+    pyautogui.press("tab")
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(str(tipo))
+    pyautogui.press("tab")
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria))
+    pyautogui.press("tab")
+    preco = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preco))
+    pyautogui.press("tab")
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(str(obs))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
+
+
